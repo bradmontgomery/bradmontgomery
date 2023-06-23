d2521af91c5dd1c2d2d3232070dedb563cc11a6f
Brad Montgomery <brad@bradmontgomery.net>
2023-06-23 01:38:56 +0000

    added our website

---
+++ docs/app.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+  
+    const btn = document.getElementById("btn");
+    btn.addEventListener("click", (event) => {
+      document.querySelector("p").className = "hilite";
+    });
+  });
+  
